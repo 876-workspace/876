7360f8f27a2d036fabaf65f1f37c55153eb88244
876-workspace <raheemforschool@gmail.com>
2026-08-24 19:40:29 -0500

    feat(crm): expose customer client

--- apps/crm/src/lib/client/index.ts
+++ apps/crm/src/lib/client/index.ts
@@ -1,5 +1,7 @@
+import { customers } from './customers'
 import { onboarding } from './onboarding'
 
-export const client = { onboarding }
+export const client = { onboarding, customers }
+export { customers } from './customers'
 export { onboarding } from './onboarding'
 export type { ClientResult } from './request'
