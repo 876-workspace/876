dca3de355939be167e9c592acbffd85765a8c056
876-workspace <raheemforschool@gmail.com>
2026-08-27 08:49:10 -0500

    feat(app-access): export provisioning permission lookup

--- apps/api/src/modules/app-access/index.ts
+++ apps/api/src/modules/app-access/index.ts
@@ -1,5 +1,8 @@
 export { registerAppAccessRoutes } from './app-access.routes'
-export { findOrgAppRoleForAccess } from './app-access-lookup.service'
+export {
+  findOrgAppRoleForAccess,
+  listAppPermissionKeysForProvisioning,
+} from './app-access-lookup.service'
 export { ensureAppMembershipForProvisioning } from './app-access-provisioning.service'
 export {
   isEntitled,
