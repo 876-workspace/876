ed187d339d7975d6a5541a0154ab02d430770389
876-workspace <raheemforschool@gmail.com>
2026-08-28 18:02:30 -0500

    feat(core-errors): export CRM error catalog

--- packages/core/src/lib/errors/index.ts
+++ packages/core/src/lib/errors/index.ts
@@ -1,2 +1,3 @@
 export { APP_ACCESS_ERRORS } from './app-access'
+export { CRM_ERRORS, type CrmErrorCode } from './crm'
 export { ERRORS, getError, isError, isErrorCode, toAppError } from './helpers'
