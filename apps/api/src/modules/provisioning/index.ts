7e75589a5180098424f08ba8c19fbba5179d81d8
876-workspace <raheemforschool@gmail.com>
2026-08-31 11:12:01 -0500

    feat(provisioning): expose import contract

--- apps/api/src/modules/provisioning/index.ts
+++ apps/api/src/modules/provisioning/index.ts
@@ -1,3 +1,18 @@
 export { createProvisioningRouter } from './provisioning.routes'
 export { createProvisioningResourceRouter } from './provisioning-resource.routes'
 export { createProvisioningSetupPolicyRouter } from './provisioning-setup-policy.routes'
+export {
+  provisioningImportSpecificationSchema,
+  type ProvisioningImportSpecification,
+} from './provisioning-import.schemas'
+export {
+  buildApplicationImportDraft,
+  buildFinanceImportDraft,
+  buildOrganizationImportDraft,
+  buildSetupPolicy,
+} from './provisioning-import.builders'
+export {
+  importProvisioningSpecification,
+  type ProvisioningImportDependencies,
+  type ProvisioningImportSummary,
+} from './provisioning-import.service'
