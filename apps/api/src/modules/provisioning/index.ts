ad98fc7ee26390c803813da7ca160763a111fc1e
876-workspace <raheemforschool@gmail.com>
2026-08-31 10:53:16 -0500

    feat(provisioning): export resource CRUD router

--- apps/api/src/modules/provisioning/index.ts
+++ apps/api/src/modules/provisioning/index.ts
@@ -1,2 +1,3 @@
 export { createProvisioningRouter } from './provisioning.routes'
+export { createProvisioningResourceRouter } from './provisioning-resource.routes'
 export { createProvisioningSetupPolicyRouter } from './provisioning-setup-policy.routes'
