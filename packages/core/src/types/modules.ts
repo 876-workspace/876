4f175e6d8c3c7f07eb45bd3763da0e256636cee7
876-workspace <raheemforschool@gmail.com>
2026-09-09 20:34:29 -0500

    feat(core-modules): define canonical module registry types

---
+++ packages/core/src/types/modules.ts
@@ -0,0 +1,16 @@
+export interface AppModuleDefinition {
+  readonly key: string
+  readonly label: string
+  readonly description: string
+}
+
+export interface AppModuleRegistry<
+  TApp extends string = string,
+  TModules extends readonly AppModuleDefinition[] = readonly AppModuleDefinition[],
+> {
+  readonly app: TApp
+  readonly modules: TModules
+}
+
+export type AppModuleKey<TRegistry extends AppModuleRegistry> =
+  TRegistry['modules'][number]['key']
