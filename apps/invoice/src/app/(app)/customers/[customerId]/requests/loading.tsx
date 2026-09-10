39df276254f67a04fc1989856de5177245f29672
876-workspace <raheemforschool@gmail.com>
2026-09-10 14:42:03 -0500

    perf(invoice-customers): skeleton request tab loading

---
+++ apps/invoice/src/app/(app)/customers/[customerId]/requests/loading.tsx
@@ -0,0 +1,5 @@
+import { CustomerTimelinePanelSkeleton } from '@876/billing-ui/panels/customer-timeline-panel'
+
+export default function Loading() {
+  return <CustomerTimelinePanelSkeleton title="Requests" />
+}
