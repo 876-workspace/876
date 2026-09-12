e73409323eb230279cbc622e398068553de326d8
876-workspace <raheemforschool@gmail.com>
2026-09-11 21:19:00 -0500

    fix(console): title notepad widget

--- apps/console/src/app/(app)/widgets/notepad/page.tsx
+++ apps/console/src/app/(app)/widgets/notepad/page.tsx
@@ -2,6 +2,8 @@ import { notepadWidgetMetadata } from '@876/widgets'
 
 import { WidgetOverview } from '@/features/widgets/components/widget-overview'
 
+export const metadata = { title: 'Notepad' }
+
 export default function NotepadWidgetPage() {
   return <WidgetOverview widget={notepadWidgetMetadata} />
 }
