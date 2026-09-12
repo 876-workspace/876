51007d103b7464a4c45f8c8c14de37a01dbc1581
876-workspace <raheemforschool@gmail.com>
2026-09-12 15:50:13 -0500

    feat(banking): export Core directory gateway

---
+++ apps/billing-api/src/providers/core-directory/index.ts
@@ -0,0 +1,7 @@
+export { HttpCoreDirectoryGateway } from './client'
+export {
+  CoreDirectoryUnavailableError,
+  type CoreDirectoryBank,
+  type CoreDirectoryBranch,
+  type CoreDirectoryGateway,
+} from './types'
