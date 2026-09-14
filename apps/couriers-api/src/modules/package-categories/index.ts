63444a33d16e27474a9862e6e2245d6c7461d3de
876-workspace <raheemforschool@gmail.com>
2026-09-14 15:33:49 -0500

    feat(couriers-api): expose package category module

---
+++ apps/couriers-api/src/modules/package-categories/index.ts
@@ -0,0 +1,6 @@
+export { createPackageCategoriesRouter } from './package-categories.routes'
+export {
+  reconcileProvisionedPackageCategories,
+  requireActivePackageCategory,
+} from './package-categories.service'
+export type { ProvisionedPackageCategory } from './package-categories.schemas'
