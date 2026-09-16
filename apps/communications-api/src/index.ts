949d96c07ebe40413d3796d6fe5f00a295145828
876-workspace <raheemforschool@gmail.com>
2026-09-15 22:05:32 -0500

    fix(communications-api): export Vercel Express handler

--- apps/communications-api/src/index.ts
+++ apps/communications-api/src/index.ts
@@ -1,2 +1,6 @@
-export { createApp } from './application.js'
+import { createApp } from './application.js'
+
 export { COMMUNICATIONS_ERRORS } from './http/errors.js'
+export { createApp }
+
+export default createApp()
